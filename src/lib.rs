e547cd9f2aa4541b6622f96dc18dc2ebd9865589
av1ctor <av1ctortv@gmail.com>
2023-12-24 10:18:27 -0300

    fix: typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /* 
  * three.rs: three.js "ported" to Rust
  * three.js copyright 2010-2023 three.js authors, released under MIT license
- * copyright 2023 André Vicentini (https://github.com/av1ctor), release under MIT license
+ * copyright 2023 André Vicentini (https://github.com/av1ctor), released under MIT license
  */
 
 pub mod math;
